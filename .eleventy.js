16ef84046e48227d7751128190b95e700b1d3049
Mohammed Zeeshan <work.zeeshan@gmail.com>
2021-12-11 09:53:19 +0530

    feat: layouts 🎉

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addFilter("debugger", (...args) => {
+      console.log(...args)
+      debugger;
+    })
     return {
       dir: {
         input: "src",
         output: "public",
+        includes: "_includes",
+        layouts: "_layouts"
       },
     };
   };
